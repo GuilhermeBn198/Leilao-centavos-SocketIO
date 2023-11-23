d223a722f9b848d339787c30e03ea20689e4cc5b
Guilherme Bernardo <bguilherme51@gmail.com>
2023-11-22 21:45:38 -0400

    user sign in and sign up

--- backend/index.js
+++ backend/index.js
@@ -1,44 +1,111 @@
-const express = require('express');
-const http = require('http');
-const socketIo = require('socket.io');
-require('dotenv').config()
-const { createClient } = require('@supabase/supabase-js')
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+require("dotenv").config();
+const { createClient } = require("@supabase/supabase-js");
 // imports
 
-
 const app = express();
+app.use(express.json());
+
 const server = http.createServer(app);
 const io = socketIo(server);
 // declaração das variaveis do servidor
 
-const SUPABASE_URL = process.env.SUPABASE_URL
-const SUPABASE_ANON_KEY = process.env.SUPABASE_ANON_KEY
+const SUPABASE_URL = process.env.SUPABASE_URL;
+const SUPABASE_ANON_KEY = process.env.SUPABASE_ANON_KEY;
 // usando variaveis de ambiente
 
-const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
+const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 // instanciando a conexão com o servidor
 
+//rest api
 
-io.on('connection', (socket) => {
-    console.log('a user connected');
+app.post("/users/login", async (req, res) => {
+    //login
+    try {
+        const { email, password } = req.body;
+        const { user, error } = await supabase.auth.signInWithPassword({
+            email,
+            password,
+        });
+    
+        if (error) {
+            throw error;
+        }
+        return res.status(200).send({ message: "usuário foi conectado!", user});
+    } catch (error) {
+        return res.status(400).send("Failed to log in");
+    }
+});
 
-    socket.on('disconnect', () => {
-        console.log('user disconnected');
+app.post("/users/signup", async (req, res) => {
+    // sign up
+    try {
+        const { name, cpf, password, email, cent_numbers } = req.body;
+        const { data, error } = await supabase.auth.signUp({
+            email: email,
+            password: password,
+            options: {
+                data: {
+                    name,
+                    cpf,
+                    cent_numbers: 10,
+                },
+            },
+        });
+
+        if (error) {
+            throw error;
+        }
+
+        return res
+            .status(200)
+            .send({ message: "Successfully signed up", data });
+    } catch (error) {}
+    console.error("Error creating user:", error);
+    return res.status(500).send("Failed to create user");
+});
+
+app.post("/users/forgot_password", async (req, res) => {});
+
+app.get("/users/:id", async (req, res) => {
+    //get
+    const { id } = req.params;
+    const { data, error } = await supabase.from("users").select("*");
+    // .eq('id', id)
+    // .single();
+
+    if (error) {
+        console.error("Error fetching user:", error);
+        res.status(500).send("Failed to fetch user");
+    } else {
+        res.send(data);
+    }
+});
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//socket api
+io.on("connection", (socket) => {
+    console.log("a user connected");
+
+    socket.on("disconnect", () => {
+        console.log("user disconnected");
     });
 
-    socket.on('chat message', async (msg) => {
+    socket.on("chat message", async (msg) => {
         const { data, error } = await supabase
-            .from('messages')
+            .from("messages")
             .insert([{ message: msg }]);
 
         if (error) {
-            console.error('Error inserting message:', error);
+            console.error("Error inserting message:", error);
         } else {
-            io.emit('chat message', msg);
+            io.emit("chat message", msg);
         }
     });
 });
 
 server.listen(3000, () => {
-    console.log('listening on *:3000');
+    console.log("listening on *:3000");
 });
