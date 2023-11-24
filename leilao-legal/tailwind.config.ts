e8cbf94abae1c835dcb8591fd72e8a8a4a7c6616
Guilherme Bernardo <bguilherme51@gmail.com>
2023-11-24 11:05:34 -0400

    new colors

--- leilao-legal/tailwind.config.ts
+++ leilao-legal/tailwind.config.ts
@@ -1,23 +1,30 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    content: [
+        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            backgroundImage: {
+                "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+                "gradient-conic":
+                    "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+            },
+            colors: {
+                "dark-slate-gray": "#2b4141ff",
+                'periwinkle': "#d4dcffff",
+                "indian-red": "#d16666ff",
+                "black-bean": "#440d0fff",
+                "caput-mortuum": "#551b14ff",
+            },
+        },
+        fontFamily: {
+            logo: ["Lobster", "Josefin Slab"],
+        },
     },
-    fontFamily: {
-      'logo': ['Lobster', 'Josefin Slab']
-    }
-  },
-  plugins: [],
-}
-export default config
+    plugins: [],
+};
+export default config;
