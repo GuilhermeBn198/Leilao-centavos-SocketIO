638ad47e4bdce88ad0973e3521bab5242e753a13
Guilherme Bernardo <bguilherme51@gmail.com>
2023-11-24 12:40:57 -0400

    more adjusts and props

--- leilao-legal/src/components/Logo.tsx
+++ leilao-legal/src/components/Logo.tsx
@@ -3,8 +3,10 @@ import "../app/globals.css";
 
 function Logo() {
     return (
-        <div className="flex h-24 justify-center items-center font-logo text-6xl text-periwinkle">
-            <AttachMoneyIcon className="text-6xl text-periwinkle"/> <h3 className="text-periwinkle">Leilão Legal DCC App</h3><AttachMoneyIcon className="text-6xl text-periwinkle" />
+        <div className="flex h-24 justify-center items-center font-logo text-5xl text-periwinkle md:text-5xl sm:text-2xl">
+            <AttachMoneyIcon className="text-2xl md:text-5xl sm:text-2xl text-periwinkle" />{" "}
+            <h1>Leilão Legal DCC App</h1>
+            <AttachMoneyIcon className="text-2xl md:text-5xl sm:text-2xl text-periwinkle" />
         </div>
     );
 }
--- leilao-legal/src/components/header.tsx
+++ leilao-legal/src/components/header.tsx
@@ -2,11 +2,12 @@ import Image from "next/image";
 import Link from "next/link";
 import AccountBoxIcon from "@mui/icons-material/AccountBox";
 import Logo from "./Logo";
+import classNames from "classnames";
 
 function Header() {
     return (
         <div className="flex bg-dark-slate-gray h-24 justify-between p-4 items-center ">
-            <div className="flex justify-center h-15 w-18 ml-2">
+            <div className="flex justify-center w-24 ml-2">
                 <Link href="/">
                     <Image
                         src="/logo-dcc-01.png"
@@ -19,7 +20,7 @@ function Header() {
             </div>
             <Logo />
             <Link href="/" className="mr-4">
-                <p className="text-periwinkle text-lg">
+                <p className="text-periwinkle text-lg font-bold ">
                     Minha Conta <AccountBoxIcon className="w-12 h-12" />
                 </p>
             </Link>
