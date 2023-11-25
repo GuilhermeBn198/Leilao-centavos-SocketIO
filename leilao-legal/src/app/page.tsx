2a5a7c684c16b7ef6cc811b2f7e98472b09b70b7
Guilherme Bernardo <bguilherme51@gmail.com>
2023-11-25 00:19:11 -0400

    reiniciando a aplicação, sem carrousel

--- leilao-legal/src/app/page.tsx
+++ leilao-legal/src/app/page.tsx
@@ -1,88 +1,27 @@
 "use client";
 
-import { useState } from "react";
 import Image from "next/image";
 import Footer from "@/components/footer";
 import Header from "@/components/header";
-import ItemCarrousel from "@/components/ItemCarrousel";
 import "./globals.css";
 
 //imports
 
-interface Item {
-    id: number;
-    name: string;
-    valor: number;
-    image: string;
-    descricao: string;
-    name_user: string;
-  }
+export default async function Home() {
+    async function getData() {
+        const res = await fetch('http://localhost:3000/items')
+        const data = await res.json()
 
-  interface MyData {
-    items: Item[];
-  }
+        return data
+      }
 
-  interface HomeProps {
-    data: MyData;
-  }
-
-
-
-
-export default function Home() {
-    
-    const images = [
-        "/peixes_fish2.jpg",
-        "/playstation.jpg",
-        "/sonic.jpg",
-        "/furador_de_coco.jpg",
-        "/cat.jpg",
-    ];
-    
-    async function getServerSideProps() {
-        try {
-            const res = await fetch('http://localhost:3000/items');
-            const data = await res.json();
-            return {
-                props: { data }, // will be passed to the page component as props
-            };
-        } catch (error) {
-            console.log(error)
-        }
-     }
-    
-     console.log(getServerSideProps())
-    
-     return (
-        <main className="flex w-full h-screen flex-col justify-between ">
+    console.log(await getData())
+    return (
+        <main className="flex w-full h-screen flex-col justify-between overflow-hidden">
             <Header />
-            <div className="overflow w-screen  lg:w-1/3 mx-auto my-2">
-                <ItemCarrousel loop>
-                    {images.map((src, i) => {
-                        return (
-                            // 👇 style each individual slide.
-                            // relative - needed since we use the fill prop from next/image component
-                            // h-64 - arbitrary height
-                            // flex[0_0_100%]
-                            //   - shorthand for flex-grow:0; flex-shrink:0; flex-basis:100%
-                            //   - we want this slide to not be able to grow or shrink and take up 100% width of the viewport.
-                            <div
-                                className="relative h-96 flex-[0_0_100%]"
-                                key={i}
-                            >
-                                {/* use object-cover + fill since we don't know the height and width of the parent */}
-                                <Image
-                                    src={src}
-                                    fill
-                                    className="object-cover"
-                                    alt="alt"
-                                />
-                            </div>
-                        );
-                    })}
-                </ItemCarrousel>
+            <div className=" flex w-2/3 h-2/3 mx-auto my-2 bg-slate-600">
+                
             </div>
-
             <Footer />
         </main>
     );
