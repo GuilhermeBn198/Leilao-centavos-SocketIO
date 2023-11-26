b50a528f1ee3ffa15dcb62fa3180bd6e1e47cf4f
Guilherme Bernardo <bguilherme51@gmail.com>
2023-11-26 10:48:04 -0400

    restar

--- leilao-legal/tailwind.config.ts
+++ leilao-legal/tailwind.config.ts
@@ -1,30 +1,20 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
-    content: [
-        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    ],
-    theme: {
-        extend: {
-            backgroundImage: {
-                "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-                "gradient-conic":
-                    "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-            },
-            colors: {
-                "dark-slate-gray": "#2b4141ff",
-                'periwinkle': "#d4dcffff",
-                "indian-red": "#d16666ff",
-                "black-bean": "#440d0fff",
-                "caput-mortuum": "#551b14ff",
-            },
-        },
-        fontFamily: {
-            logo: ["Lobster", "Josefin Slab"],
-        },
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
     },
-    plugins: [],
-};
-export default config;
+  },
+  plugins: [],
+}
+export default config
