1ef13043c7f5658ca4256d06e93b422d5b8e327c
Guilherme Bernardo <bguilherme51@gmail.com>
2023-11-27 00:06:33 -0400

    new servidor

--- leilao-legal/app/page.tsx
+++ leilao-legal/app/page.tsx
@@ -3,29 +3,28 @@ import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import React from 'react';
  
-const items: Array<{ nome: string; descricao: string; valor: number; image: string; }> = [
-  { nome: 'Item 1', descricao: 'This is item 1', valor: 0.10, image: 'https://pbs.twimg.com/media/F9ske5rWAAAMbNY.jpg' },
-  { nome: 'Item 2', descricao: 'This is item 2', valor: 0.10, image: 'https://pbs.twimg.com/media/F9ske5rWAAAMbNY.jpg' },
-  { nome: 'Item 3', descricao: 'This is item 3', valor: 0.10, image: 'https://pbs.twimg.com/media/F9ske5rWAAAMbNY.jpg' },
-  { nome: 'Item 2', descricao: 'This is item 2', valor: 0.10, image: 'https://pbs.twimg.com/media/F9ske5rWAAAMbNY.jpg' },
-  { nome: 'Item 3', descricao: 'This is item 3', valor: 0.10, image: 'https://pbs.twimg.com/media/F9ske5rWAAAMbNY.jpg' },
-  { nome: 'Item 2', descricao: 'This is item 2', valor: 0.10, image: 'https://pbs.twimg.com/media/F9ske5rWAAAMbNY.jpg' },
-  { nome: 'Item 3', descricao: 'This is item 3', valor: 0.10, image: 'https://pbs.twimg.com/media/F9ske5rWAAAMbNY.jpg' },
-  { nome: 'Item 2', descricao: 'This is item 2', valor: 0.10, image: 'https://pbs.twimg.com/media/F9ske5rWAAAMbNY.jpg' },
-  { nome: 'Item 3', descricao: 'This is item 3', valor: 0.10, image: 'https://pbs.twimg.com/media/F9ske5rWAAAMbNY.jpg' },
-  { nome: 'Item 3', descricao: 'This is item 3aaaaaaaaaa', valor: 0.10, image: 'https://pbs.twimg.com/media/F9ske5rWAAAMbNY.jpg' },
-  { nome: 'Item 3', descricao: 'This is item 3', valor: 0.10, image: 'https://pbs.twimg.com/media/F9ske5rWAAAMbNY.jpg' },
-  { nome: 'Item 3', descricao: 'This is item 3', valor: 0.10, image: 'https://pbs.twimg.com/media/F9ske5rWAAAMbNY.jpg' },
-  { nome: 'Item 3', descricao: 'This is item 3', valor: 0.10, image: 'https://pbs.twimg.com/media/F9ske5rWAAAMbNY.jpg' },
-  { nome: 'Item 3', descricao: 'This is item 3', valor: 0.10, image: 'https://pbs.twimg.com/media/F9ske5rWAAAMbNY.jpg' },
-  { nome: 'Item 3', descricao: 'This is item 3', valor: 0.10, image: 'https://pbs.twimg.com/media/F9ske5rWAAAMbNY.jpg' },
-  { nome: 'Item 3', descricao: 'This is item 3', valor: 0.10, image: 'https://pbs.twimg.com/media/F9ske5rWAAAMbNY.jpg' },
-  // ... more items
+const items: Array<{ nome_prod: string; descricao: string; valor: number; image: string; ultimo_lance:string; dono: string; }> = [
+  { nome_prod: 'Item 1', descricao: 'This is item 1', valor: 0.10, image: 'https://pbs.twimg.com/media/F9ske5rWAAAMbNY.jpg', ultimo_lance: "rock", dono: "o rei"},
+  { nome_prod: 'Item 2', descricao: 'This is item 2', valor: 0.10, image: 'https://pbs.twimg.com/media/F9ske5rWAAAMbNY.jpg', ultimo_lance: "rock", dono: "o rei" },
+  { nome_prod: 'Item 3', descricao: 'This aaaaaaaais item 3', valor: 0.10, image: 'https://pbs.twimg.com/media/F9ske5rWAAAMbNY.jpg', ultimo_lance: "rock", dono: "o rei" },
+  { nome_prod: 'Item 2', descricao: 'This is item 2', valor: 0.10, image: 'https://pbs.twimg.com/media/F9ske5rWAAAMbNY.jpg', ultimo_lance: "rock", dono: "o rei" },
+  { nome_prod: 'Item 3', descricao: 'This is item 3', valor: 0.10, image: 'https://pbs.twimg.com/media/F9ske5rWAAAMbNY.jpg', ultimo_lance: "rock", dono: "o rei" },
+  { nome_prod: 'Item 2', descricao: 'This is item 2', valor: 0.10, image: 'https://pbs.twimg.com/media/F9ske5rWAAAMbNY.jpg', ultimo_lance: "rock", dono: "o rei" },
+  { nome_prod: 'Item 3', descricao: 'This is item 3', valor: 0.10, image: 'https://pbs.twimg.com/media/F9ske5rWAAAMbNY.jpg', ultimo_lance: "rock", dono: "o rei" },
+  { nome_prod: 'Item 2', descricao: 'This is item 2', valor: 0.10, image: 'https://pbs.twimg.com/media/F9ske5rWAAAMbNY.jpg', ultimo_lance: "rock", dono: "o rei" },
+  { nome_prod: 'Item 3', descricao: 'This is item 3', valor: 0.10, image: 'https://pbs.twimg.com/media/F9ske5rWAAAMbNY.jpg', ultimo_lance: "rock", dono: "o rei" },
+  { nome_prod: 'Item 3', descricao: 'This is item 3 aaaaaaasdasd asd asdasd asda sd asd asds aaaaa', valor: 0.10, image: 'https://pbs.twimg.com/media/F9ske5rWAAAMbNY.jpg', ultimo_lance: "rock", dono: "o rei" },
+  { nome_prod: 'Item 3', descricao: 'This is item 3', valor: 0.10, image: 'https://pbs.twimg.com/media/F9ske5rWAAAMbNY.jpg', ultimo_lance: "rock", dono: "o rei" },
+  { nome_prod: 'Item 3', descricao: 'This is item 3', valor: 0.10, image: 'https://pbs.twimg.com/media/F9ske5rWAAAMbNY.jpg', ultimo_lance: "rock", dono: "o rei" },
+  { nome_prod: 'Item 3', descricao: 'This is item 3', valor: 0.10, image: 'https://pbs.twimg.com/media/F9ske5rWAAAMbNY.jpg', ultimo_lance: "rock", dono: "o rei" },
+  { nome_prod: 'Item 3', descricao: 'This is item 3', valor: 0.10, image: 'https://pbs.twimg.com/media/F9ske5rWAAAMbNY.jpg', ultimo_lance: "rock", dono: "o rei" },
+  { nome_prod: 'Item 3', descricao: 'This is item 3', valor: 0.10, image: 'https://pbs.twimg.com/media/F9ske5rWAAAMbNY.jpg', ultimo_lance: "rock", dono: "o rei" },
+  { nome_prod: 'Item 3', descricao: 'This is item 3', valor: 0.10, image: 'https://pbs.twimg.com/media/F9ske5rWAAAMbNY.jpg', ultimo_lance: "rock", dono: "o rei" },
  ];
 
 export default function Home() {
     return (
-        <main className="flex w-screen h-screen flex-col justify-center items-center  ">
+        <main className="flex w-screen h-screen flex-col justify-center items-center mt-14 ">
             <Header />
             <DashBoard items={items} />
             <Footer />
